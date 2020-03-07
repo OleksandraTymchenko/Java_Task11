ee51394fd93494b55b38744f7d9d2f1b6ce5543c
Oleksandra Tymchenko <oleksatym15@gmail.com>
2020-03-07 14:36:07 +0200

    modified

--- src/alex/com/task02/Main.java
+++ src/alex/com/task02/Main.java
@@ -1,9 +1,5 @@
 package alex.com.task02;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
